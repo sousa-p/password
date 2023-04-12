33f98a7829d5399be6e7ae0b12097fb0a93a3e55
sousa-p <p.soousa7@gmail.com>
2023-04-12 18:44:40 -0300

    Feature funções, e o icone de seta

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
-const password = document.getElementById('password');
-const result = document.getElementById('result');
+const inputPassword = document.getElementById('password');
+const inputResult   = document.getElementById('result');
+
+const btnCopy   = document.getElementById('copy');
+const btnEncode = document.getElementById('encode');
+
+function encode (data) {
+  let encoded;
+  encoded = btoa(data);
+  return encoded;
+}
+
+btnEncode.addEventListener('click', () => {
+  inputResult.value = encode(inputPassword.value);
+});
+
+inputPassword.addEventListener('keydown', (event) => {
+  if (event.key === 'Key') {
+    inputResult.value = encode(inputPassword.value);
+  }
+});
+
+btnCopy.addEventListener('click', () => {
+  navigator.clipboard.writeText(inputResult.value);
+});
