fa6bd75537a026bcdf0cee56483c06020a36484e
sousa-p <p.soousa7@gmail.com>
2023-07-14 11:01:47 -0300

    Encode em sha256

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
-const inputPassword = document.getElementById('password');
-const inputResult   = document.getElementById('result');
+const btnCopy = document.getElementById("copy");
+const btnEncode = document.getElementById("encode");
+const inputResult = document.getElementById("result");
+const inputPassword = document.getElementById("password");
 
-const btnCopy   = document.getElementById('copy');
-const btnEncode = document.getElementById('encode');
+btnEncode.addEventListener("click", encode);
 
-function encode (data) {
-  let encoded;
-  encoded = btoa(data);
-  return encoded;
-}
-
-btnEncode.addEventListener('click', () => {
-  inputResult.value = encode(inputPassword.value);
+inputPassword.addEventListener("keydown", (event) => {
+  inputResult.value = event.key === "Enter" ? encode() : inputResult.value;
 });
 
-inputPassword.addEventListener('keydown', (event) => {
-  console.log(event.key)
-  inputResult.value = (event.key === 'Enter') ? encode(inputPassword.value) : inputResult.value;
-});
-
-btnCopy.addEventListener('click', () => {
+btnCopy.addEventListener("click", () => {
+  inputResult.focus();
   inputResult.select();
+  try {
+    // Somente alguns navegadores
+    document.execCommand("copy");
+  } catch (_) {
+    console.error(_);
+  }
   inputResult.setSelectionRange(0, inputResult.value.length);
   navigator.clipboard.writeText(inputResult.value);
 });
+
+async function encode() {
+  const utf8 = new TextEncoder().encode(inputPassword.value);
+  return crypto.subtle.digest("SHA-256", utf8).then((hashBuffer) => {
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((bytes) => bytes.toString(16).padStart(2, "0"))
+      .join("");
+    inputResult.value = hashHex;
+  });
+}
