4c45500d2db2ee70f4460c8e2149f6c25350d46a
sousa-p <p.soousa7@gmail.com>
2023-09-07 19:43:21 -0300

    Feature configurações avançadas

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # Password
-Password Encoder to Base64 using JS and Tailwind CSS
+
+Password Encoder using JS, Tailwind CSS and Bootstrap
 
 URL: https://sousa-p.github.io/password/
 
 ## Desktop
+
 ![image](https://user-images.githubusercontent.com/97417230/231593533-9cc7bda6-d0c2-4fba-a13a-2a8e634392f1.png)
+
 ## Tablet
+
 ![image](https://user-images.githubusercontent.com/97417230/231593739-f0c1f557-7caa-4606-b6e2-62cf17d9e809.png)
+
 ## Smartphone
+
 <p align="center">
   <img src="https://user-images.githubusercontent.com/97417230/231593879-89a2e131-4eea-4d7b-bb06-f5c7659a35d9.png" />
 </p>
--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 const btnCopy = document.getElementById("copy");
 const btnEncode = document.getElementById("encode");
+const btnSave = document.getElementById("save");
 const inputResult = document.getElementById("result");
 const inputPassword = document.getElementById("password");
 
+const base64 = document.getElementById("base64");
+const capital = document.getElementById("capital_letters");
+const special = document.getElementById("special_characters");
+
+
 btnEncode.addEventListener("click", encode);
+btnSave.addEventListener("click", encode);
 
 inputPassword.addEventListener("keydown", (event) => {
   inputResult.value = event.key === "Enter" ? encode() : inputResult.value;
@@ -12,23 +19,45 @@ inputPassword.addEventListener("keydown", (event) => {
 btnCopy.addEventListener("click", () => {
   inputResult.focus();
   inputResult.select();
-  try {
-    // Somente alguns navegadores
-    document.execCommand("copy");
-  } catch (_) {
-    console.error(_);
-  }
+  document.execCommand("copy");
   inputResult.setSelectionRange(0, inputResult.value.length);
   navigator.clipboard.writeText(inputResult.value);
 });
 
 async function encode() {
+  if (base64.checked)
+    return inputResult.value = addConfigs(btoa(inputPassword.value));
+  
   const utf8 = new TextEncoder().encode(inputPassword.value);
   return crypto.subtle.digest("SHA-256", utf8).then((hashBuffer) => {
     const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray
+    let hashHex = hashArray
       .map((bytes) => bytes.toString(16).padStart(2, "0"))
       .join("");
-    inputResult.value = hashHex;
+    
+    
+    inputResult.value = addConfigs(hashHex);
   });
+}
+
+
+function addConfigs(str) {
+  let result = str;
+
+  if (capital.checked) {
+    let match, idFirstLetter, startString, endString;
+
+    match = /[a-z]/.exec(result);
+    
+    idFirstLetter = match.index;
+    
+    startString = (idFirstLetter > 0) ? result.slice(0, idFirstLetter) : '';
+    capitalLetter = result.charAt(idFirstLetter).toUpperCase();
+    endString = result.slice(idFirstLetter+1);
+
+    result = startString + capitalLetter + endString;
+  };
+  if (special.checked) result = '@' + result;
+
+  return result
 }
